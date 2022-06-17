b427322b2ffce5324888c61c5aa31e8d4e3d26fe
EMurrell <82235988+EMurrell@users.noreply.github.com>
2022-06-17 13:24:11 -0400

    Add Footer component

--- src/lib/graphql-queries.js
+++ src/lib/graphql-queries.js
@@ -43,3 +43,37 @@ export const projectQuery = gql`
 		...ProjectDetails
 	}
 `;
+
+const POST_FRAGMENT = gql`
+	fragment PostDetails on Post {
+		title
+		slug
+		date
+		content
+		tags
+		coverImage {
+			url
+		}
+		authors {
+			name
+		}
+	}
+`;
+
+export const postsQuery = gql`
+	${POST_FRAGMENT}
+	query GetPosts {
+		posts {
+			...PostDetails
+		}
+	}
+`;
+
+export const postQuery = gql`
+	${POST_FRAGMENT}
+	query GetPost($slug: String!) {
+		post(where: { slug: $slug }) {
+			...PostDetails
+		}
+	}
+`;
